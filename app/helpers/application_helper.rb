55963cf0ea2585e10ea22da25d35f47c5f167b95
Jakub Matrac <jakubmatrac@gmail.com>
2017-07-22 18:22:12 +0200

    Add rating to review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def active?(*paths)
    'active' if paths.any?{ |path| current_page?(path) }
  end
+
+
+ def star(a, b)
+   if a >= b+1
+   'btn-warning'
+  else
+    'btn-default btn-grey'
+  end
+ end
+
 end

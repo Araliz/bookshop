8c46597e7f5354cdf229a6da8e194cb8a4b17f6c
Jakub Matrac <jakubmatrac@gmail.com>
2017-08-30 12:36:13 +0200

    Setup Rspec

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,15 @@
 class ReviewsController < ApplicationController
   def create
-    @review = current_user.reviews.build(micropost_params)
+    @review = current_user.reviews.build(review_params)
     @review.book_id = params[:book_id]
     if @review.save
       redirect_to book_path(params[:book_id]), notice: "Review created!"
     end
   end
 
-  def method_name
-
-  end
 
   private
-  def micropost_params
+  def review_params
     params.require(:review).permit(:content, :title, :rating)
   end
 end
